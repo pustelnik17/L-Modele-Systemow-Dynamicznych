6878cbce724b9535076a341744f62d85ba502a9a
FoxFate <solembum.pospolity@gmail.com>
2024-05-06 23:00:20 +0200

    Generate plots

--- Lista3/Solutions.py
+++ Lista3/Solutions.py
@@ -1,7 +1,38 @@
 from Solvers import ODEINTMethod, EulerMethod, Printer
 
-
-# Printer.print(EulerMethod.lorentz, [0.01, 25, 1, 1, 1, 10, 8/3, 28])
-# Printer.print(ODEINTMethod.lorentz, [0.01, 25, 1, 1, 1, 10, 8/3, 28])
-# Printer.print(EulerMethod.lotkaVolterra, [0.01, 25, 2, 1, 1.2, 0.6, 0.3, 0.8])
-Printer.print(ODEINTMethod.lotkaVolterra, [0.01, 25, 2, 1, 1.2, 0.6, 0.3, 0.8])
+dt = 0.01
+Printer.print(EulerMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz EulerMethod {dt}")
+Printer.print(ODEINTMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz ODEINT {dt}")
+Printer.print(EulerMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra EulerMethod {dt}")
+Printer.print(ODEINTMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra ODEINT {dt}")
+dt = 0.02
+Printer.print(EulerMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz EulerMethod {dt}")
+Printer.print(ODEINTMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz ODEINT {dt}")
+Printer.print(EulerMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra EulerMethod {dt}")
+Printer.print(ODEINTMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra ODEINT {dt}")
+dt = 0.001
+Printer.print(EulerMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz EulerMethod {dt}")
+Printer.print(ODEINTMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz ODEINT {dt}")
+Printer.print(EulerMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra EulerMethod {dt}")
+Printer.print(ODEINTMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra ODEINT {dt}")
+dt = 0.001
+Printer.print(EulerMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz EulerMethod dt={dt}")
+Printer.print(ODEINTMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
+              f"Lorentz ODEINT {dt}")
+Printer.print(EulerMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra EulerMethod {dt}")
+Printer.print(ODEINTMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
+              f"LotkaVolterra ODEINT {dt}")
