98d578666d992c30d8147be3b68ffc7df6f9ade8
FoxFate <solembum.pospolity@gmail.com>
2024-05-06 23:42:03 +0200

    Fix typo

--- Lista3/Solutions.py
+++ Lista3/Solutions.py
@@ -29,7 +29,7 @@ Printer.print(ODEINTMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
               f"LotkaVolterra ODEINT {dt}")
 dt = 0.001
 Printer.print(EulerMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
-              f"Lorentz EulerMethod dt={dt}")
+              f"Lorentz EulerMethod {dt}")
 Printer.print(ODEINTMethod.lorentz, [dt, 25, 1, 1, 1, 10, 8/3, 28],
               f"Lorentz ODEINT {dt}")
 Printer.print(EulerMethod.lotkaVolterra, [dt, 25, 2, 1, 1.2, 0.6, 0.3, 0.8],
