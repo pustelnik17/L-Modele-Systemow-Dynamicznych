8cf731976b2bc3bed781254ddc3a90db4be64a06
FoxFate <solembum.pospolity@gmail.com>
2024-05-18 20:58:38 +0200

    Switch project to RLC model

--- Lista4/Solutions.py
+++ Lista4/Solutions.py
@@ -1,22 +1,6 @@
-from Solvers import Object, NBodyProblem, Printer
+from Solvers import DynamicModel, Printer, sp, np
 
-eq = NBodyProblem.SympySolver.getEquations([
-    Object(10000000, [0, 0, 0], [0, 0, 0]),
-    Object(10000000, [3, 0, 0], [0, 0, 0]),
-    Object(10000000, [0, 4, 0], [0, 0, 0]),
-])
-print(eq[0][0])
-sim = NBodyProblem.SympySolver.simulate(eq, 100, 100000)
-
-# fig = plt.figure()
-# plt.plot(sim[0][0], "red")
-# plt.plot(sim[0][1], "green")
-# plt.plot(sim[0][2], "blue")
-# plt.show()
-
-# Printer.print(sim)
-
-# for e in eq:
-#     for i in range(len(e)):
-#         print((e[i].subs("t", 1)))
-#     print()
+# args = (1, 10, 1, 10, 1, 1, 1, 0.1, 20)
+sim1 = DynamicModel.SympySolver.simulate(1, 10, 1, 3, 1, 1, 1, 0.1, 30)
+sim2 = DynamicModel.OdeIntSolver.simulate(1, 10, 1, 3, 1, 1, 1, 0.1, 30)
+Printer.print(sim1, sim2)
--- Lista4/Solvers.py
+++ Lista4/Solvers.py
@@ -1,113 +1,76 @@
+from typing import Tuple, Any
+
 import sympy as sp
 import numpy as np
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-class Object:
-    def __init__(self, mass, position=None, velocity=None):
-        if velocity is None:
-            velocity = [0, 0, 0]
-        if position is None:
-            position = [0, 0, 0]
-        self.mass = mass
-        self.pos = sp.Matrix(position)
-        self.vel = sp.Matrix(velocity)
-
-    def getMass(self):
-        return self.mass
-
-    def getPos(self):
-        return self.pos
-
-    def getVel(self):
-        return self.vel
 
 
-class NBodyProblem:
+
+
+class DynamicModel:
     class SympySolver:
         @staticmethod
-        def getEquations(objects: list) -> list[tuple]:
-            def _calculateLength(m: sp.Matrix):
-                sumT = 0
-                for k in range(len(m)):
-                    sumT += m[k] * m[k]
-                return sp.sqrt(sumT)
-
-            n = len(objects)
-            G = sp.symbols('G')
+        def RLC(q0: float, i0: float, R: float, L: float, C: float, A: float, Omega: float):
             t = sp.symbols('t')
+            q = sp.Function('q')(t)
+            i = sp.Function('i')(t)
 
-            x, y, z = [], [], []
-            for i in range(n):
-                x.append(sp.Function(f"x{i}")(t))
-                y.append(sp.Function(f"y{i}")(t))
-                z.append(sp.Function(f"z{i}")(t))
+            eq1 = sp.Eq(q.diff(t), i)
+            eq2 = sp.Eq(L * i.diff(t) + R * i + q / C, A*sp.cos(Omega*t))
 
-            equations = []
-            for i in range(n):
-                expression = [(objects[j].getMass() * (objects[i].getPos() - objects[j].getPos())) /
-                              (_calculateLength((objects[i].getPos() - objects[j].getPos())) ** 3) for j in range(n)]
-                iterativeSum = sp.Matrix(sp.Add(*expression))
-                equations.append(x[i].diff(t, t) - G * iterativeSum[0])
-                equations.append(y[i].diff(t, t) - G * iterativeSum[1])
-                equations.append(z[i].diff(t, t) - G * iterativeSum[2])
+            solution = sp.dsolve((eq1, eq2), ics={q.subs(t, 0): q0, i.subs(t, 0): i0})
 
-            ics = {
-                **{axis[0][i].subs(t, 0): objects[i].getPos()[axis[1]]
-                   for i in range(n) for axis in ((x, 0), (y, 1), (z, 2))},
-                **{axis[0][i].diff().subs(t, 0): objects[i].getVel()[axis[1]]
-                   for i in range(n) for axis in ((x, 0), (y, 1), (z, 2))}
-            }
-
-            solutions = sp.dsolve(equations, ics=ics)
-
-            return [(solutions[i].rhs.subs("G", 6.67430 * (10 ** -11)),
-                     solutions[i + 1].rhs.subs("G", 6.67430 * (10 ** -11)),
-                     solutions[i + 2].rhs.subs("G", 6.67430 * (10 ** -11)))
-                    for i in range(0, len(solutions), 3)]
+            return solution
 
         @staticmethod
-        def simulate(equations: list[tuple], dt: float, time: float):
+        def simulate(q0: float, i0: float, R: float, L: float, C: float, A: float, Omega: float, dt: float, time: float):
+            equations: list[sp.Eq] = DynamicModel.SympySolver.RLC(q0, i0, R, C, L, A, Omega)
             numberOfSteps = int(time / dt)
             systemEvolution = []
-            for obj in equations:
-                objEvolution = []
-                for axis in range(len(obj)):
-                    axisEvolution = []
-                    for step in range(numberOfSteps):
-                        axisEvolution.append(obj[axis].subs("t", dt*step))
-                    objEvolution.append(axisEvolution)
-                systemEvolution.append(objEvolution)
+            for equation in equations:
+                eqEvolution = []
+                for step in range(numberOfSteps):
+                    eqEvolution.append(equation.rhs.subs("t", dt * step))
+                systemEvolution.append(eqEvolution)
             return systemEvolution
+
+    class OdeIntSolver:
+        @staticmethod
+        def simulate(q0: float, i0: float, R: float, L: float, C: float, A: float, Omega: float, dt: float, time: float):
+            def _V(_t, a: float, omega: float):
+                return a * np.cos(omega * _t)
+
+            def _RLC(y, _t, _R, _L, _C):
+                q, i = y
+
+                dqdt = i
+                didt = (1/_L) * (_V(_t, A, Omega) - _R*i - q/_C)
+
+                return [dqdt, didt]
+
+            y0 = [q0, i0]
+            t = np.arange(0.0, time, dt)
+            solution = odeint(_RLC, y0, t, args=(R, L, C))
+            return solution.T
+
 
 class Printer:
     @staticmethod
-    def print(systemEvolution):
-        # fig = plt.figure()
-        # fig.suptitle(f"N Body Problem", fontsize="x-large")
-        # ax = plt.axes(projection='3d')
-        # for obj in systemEvolution:
-        #     ax.plot3D(obj[0], obj[1], obj[2])
-        #     ax.scatter(obj[0][0], obj[1][0], obj[2][0], c='red', marker='*', s=10)
-        # plt.show()
+    def print(systemEvolution1, systemEvolution2):
+        fig, ax = plt.subplots(1, 2)
+        fig.suptitle(f"Układ RLC", fontsize="x-large")
 
-        def update(frame):
-            scat.set_offsets([systemEvolution[0][0][frame], systemEvolution[1][0][frame]])
+        ymin, ymax = (min(*systemEvolution1[0], *systemEvolution1[1], *systemEvolution2[0], *systemEvolution2[1]),
+                      max(*systemEvolution1[0], *systemEvolution1[1], *systemEvolution2[0], *systemEvolution2[1]))
 
-            return scat,
+        ax[0].set_ylim([ymin, ymax])
+        ax[1].set_ylim([ymin, ymax])
 
-            # Inicjalizacja wykresu
+        ax[0].plot(systemEvolution1[0], "red", label="Q(t)")
+        ax[0].plot(systemEvolution1[1], "blue", label="I(t)")
+        ax[1].plot(systemEvolution2[0], "darkred", label="Q(t)")
+        ax[1].plot(systemEvolution2[1], "cornflowerblue", label="I(t)")
 
-        fig, ax = plt.subplots()
-        x = 30000
-        ax.set_xlim(-2*x, 0)  # Ustawienie limitów osi x
-        ax.set_ylim(0, 2*x)  # Ustawienie limitów osi y
-
-        # Inicjalizacja obiektu punktu
-        scat = ax.scatter(systemEvolution[0][0][0], systemEvolution[0][1][0])
-
-        # Ustawienie animacji
-        ani = animation.FuncAnimation(fig, update, frames=len(systemEvolution[0][0]), interval=50, blit=True)
-
-        # Wyświetlenie animacji
+        plt.legend()
         plt.show()
